883b364ecf80df96b6d5e6cf4e3731de05184e49
Rustle Karl <fu.jiawei@outlook.com>
2022-02-07 13:16:02 +0800

    update: 第7章_比高斯更快的计算

--- src/docs/02/07/c07_mbr.asm
+++ src/docs/02/07/c07_mbr.asm
@@ -2,20 +2,20 @@
          ;文件名：c07_mbr.asm
          ;文件说明：硬盘主引导扇区代码
          ;创建日期：2011-4-13 18:02
-         
+
          jmp near start
-	
+
  message db '1+2+3+...+100='
-        
+
  start:
-         mov ax,0x7c0           ;设置数据段的段基地址 
+         mov ax,0x7c0           ;设置数据段的段基地址
          mov ds,ax
 
          mov ax,0xb800          ;设置附加段基址到显示缓冲区
          mov es,ax
 
-         ;以下显示字符串 
-         mov si,message          
+         ;以下显示字符串
+         mov si,message
          mov di,0
          mov cx,start-message
      @g:
@@ -27,7 +27,7 @@
          inc si
          loop @g
 
-         ;以下计算1到100的和 
+         ;以下计算1到100的和
          xor ax,ax
          mov cx,1
      @f:
@@ -36,7 +36,7 @@
          cmp cx,100
          jle @f
 
-         ;以下计算累加和的每个数位 
+         ;以下计算累加和的每个数位
          xor cx,cx              ;设置堆栈段的段基地址
          mov ss,cx
          mov sp,cx
@@ -52,7 +52,7 @@
          cmp ax,0
          jne @d
 
-         ;以下显示各个数位 
+         ;以下显示各个数位
      @a:
          pop dx
          mov [es:di],dl
@@ -60,9 +60,9 @@
          mov byte [es:di],0x07
          inc di
          loop @a
-       
-         jmp near $ 
-       
+
+         jmp near $
+
 
 times 510-($-$$) db 0
                  db 0x55,0xaa
